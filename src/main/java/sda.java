debfa1fda37f1dce29ed83f6d87f834a45160163
wangshi <wanghi@qq.com>
2022-06-02 22:48:16 +0800

    test

--- src/main/java/sda.java
+++ src/main/java/sda.java
@@ -1,6 +1,7 @@
 public class sda {
     public static void main(String[] args) {
         System.out.println("as");
-        System.out.println();
+        System.out.println("dsa");
+        System.out.println("d");
     }
 }
